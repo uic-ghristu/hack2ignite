ce3c5a648048e240757680d2e59aacb31f0cd34a
abheeee03 <abhaynimbalkar03@gmail.com>
2026-08-18 14:06:29 +0530

    init hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,88 @@
-import Image from "next/image";
+import ReactLenis from 'lenis/react'
+import Image from 'next/image'
+import { CloudShader } from "@/components/ui/cloud-shader";
+import Link from 'next/link';
+import { Jersey_20 } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
-export default function Home() {
+const pixelFont = Jersey_20({
+  weight: "400"
+})
+
+
+function LandingPage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div
+      className='min-h-screen w-full'
+    >
+      <ReactLenis />
+      <div className="h-screen w-full bg-[#90C5EF] flex flex-col items-center justify-center">
+        <nav 
+        className={cn(
+          pixelFont.className,
+          'w-full absolute top-1 px-15 z-20 py-4 flex items-center justify-between'
+        )}
+        >
+          <div className="">
+              <Image
+              className='pointer-events-none'
+              src={'/main-logo.png'}
+              width={100}
+              height={100}
+              alt='logo'
+              />
+          </div>
+          <div className="flex text-white items-center justify-center gap-10">
+
+          {
+            [{name: "Prizes"}, {name: "Tracks"}, {name: "Timeline"}].map((link)=>
+              <Link key={link.name} href={"#"} className='text-2xl'>{link.name}</Link>
+          )
+        }
+        </div>
+        <div className="">
+          <button
+          className='text-2xl'
+          >
+            Register Now
+          </button>
+        </div>
+        </nav>
+        <CloudShader 
+        speed={0.7}
+        className="h-screen w-full absolute z-10" />
+        
+        {/* //main hero section  */}
+        <div className="z-20 flex items-center justify-center flex-col">
+          <Image
+            width={500}
+            height={500}
+            alt='logo'
+            src={'/main-logo.png'}
+          />
+          <div className={cn(
+            pixelFont.className,
+            "text-white text-4xl"
+          )}
+          >Where Skills Meet Innovation.</div>
+          <div className="mt-10">
+            <button className='border px-10 py-2 rounded-md flex items-center justify-center gap-2'>
+              Register on <Image height={80} width={80} alt='unstop.com' src={'/unstop.webp'}/>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen w-full bg-[#90C5EF] relative">
         <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          className='absolute bottom-0'
+          height={1000}
+          width={2000}
+          alt='bg'
+          src={'/background1.png'}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
-  );
+  )
 }
+
+export default LandingPage
